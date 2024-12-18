aa609c42520a87214945d5bd574f8452225817c1
tiagomarcuz <69879102+tiagomarcuz@users.noreply.github.com>
2024-12-18 20:31:16 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
-function Home(){
-    return <h1>TE AMO MEU AMOR - FELIZ ANIVERSÁRIO
-        SÃO OS DESEJOS MEU E DE SUA FILHA ANTÕNIA 🍰
-    </h1>
+function Home() {
+  return;
+  <h1>
+    TE AMO MEU AMOR - FELIZ ANIVERSÁRIO SÃO OS DESEJOS MEU E DE SUA FILHA
+    ANTÕNIA 🍰
+  </h1>;
 }
 
 export default Home;
